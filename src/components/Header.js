bfa679c6024773d7c442ec01226e5925d34d59ae
ddlinkz <ddlinkab@gmail.com>
2023-02-10 19:27:20 -0500

    Add .gitignore to reflect adding Netlify Add hyphen to "8-Ball Radio"

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ import '../styles.css';
 const Header = () => {
     return (
         <header>
-            <h1><Link to='/'>8 Ball Radio</Link></h1>
+            <h1><Link to='/'>8-Ball Radio</Link></h1>
             <Player url={"https://eightball.out.airtime.pro/eightball_a"}/>
             <NavBar />
         </header>
