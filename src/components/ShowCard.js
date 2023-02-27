d600b93172537c5298b8c2882b168411201a392e
Teddy Wilson <hello@teddywilson.me>
2023-02-26 22:26:43 -0500

    Show styling

--- src/components/ShowCard.js
+++ src/components/ShowCard.js
@@ -5,6 +5,7 @@ import '../styles.css'
 const ShowCard = ({ show }) => {
   return (
     <div className="show-card">
+      <div className="show-date">4/20/2023</div>
       <div className="show-title">{show.showname}</div>
       <div className="show-description">{show.description}</div>
     </div>
