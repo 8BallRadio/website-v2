3fa1ad3fba8688fb50844fe81fe8163c6565eca6
Teddy Wilson <hello@teddywilson.me>
2023-02-26 22:31:51 -0500

    show an image

--- src/components/ShowCard.js
+++ src/components/ShowCard.js
@@ -5,6 +5,7 @@ import '../styles.css'
 const ShowCard = ({ show }) => {
   return (
     <div className="show-card">
+      <img src="https://assets.technologynetworks.com/production/dynamic/images/content/364735/mxene-material-can-efficiently-mop-up-mercury-contamination-364735-1280x720.webp?cb=11899209" />
       <div className="show-date">4/20/2023</div>
       <div className="show-title">{show.showname}</div>
       <div className="show-description">{show.description}</div>
