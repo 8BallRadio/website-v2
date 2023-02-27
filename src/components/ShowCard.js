7d0f1cb4f18a622954afc375b190b91c3cb0b97b
Teddy Wilson <hello@teddywilson.me>
2023-02-27 09:41:28 -0500

    border radius on show card image

--- src/components/ShowCard.js
+++ src/components/ShowCard.js
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom'
 
 import '../styles.css'
 
+const MOCK_DATE = '4/20/2023'
+const MOCK_IMAGE =
+  'https://assets.technologynetworks.com/production/dynamic/images/content/364735/mxene-material-can-efficiently-mop-up-mercury-contamination-364735-1280x720.webp?cb=11899209'
 const MOCK_TAGS = ['grime', 'dubstep', 'techno', 'electro', 'ambient']
 
 const ShowCard = ({ show }) => {
@@ -17,8 +20,8 @@ const ShowCard = ({ show }) => {
       <Link to={show.key} relative="path">
         <div className="show-card">
           <div className="show-card-top">
-            <img src="https://assets.technologynetworks.com/production/dynamic/images/content/364735/mxene-material-can-efficiently-mop-up-mercury-contamination-364735-1280x720.webp?cb=11899209" />
-            <div className="show-date">4/20/2023</div>
+            <img src={MOCK_IMAGE} className="show-image" />
+            <div className="show-date">{MOCK_DATE}</div>
             <div className="show-title">{show.showname}</div>
             <div className="show-description">{show.description}</div>
           </div>
