7f84097eb893322334ed1a558e8110e4fca86b90
Teddy Wilson <hello@teddywilson.me>
2023-02-26 22:42:45 -0500

    add key

--- src/components/ShowCard.js
+++ src/components/ShowCard.js
@@ -2,13 +2,23 @@ import React from 'react'
 
 import '../styles.css'
 
+const MOCK_TAGS = ['grime', 'dubstep', 'techno', 'electro', 'ambient']
+
 const ShowCard = ({ show }) => {
+  const tags = MOCK_TAGS.map((tag, index) => (
+    <div className="show-tag" key={index}>
+      {tag}
+    </div>
+  ))
   return (
     <div className="show-card">
       <img src="https://assets.technologynetworks.com/production/dynamic/images/content/364735/mxene-material-can-efficiently-mop-up-mercury-contamination-364735-1280x720.webp?cb=11899209" />
       <div className="show-date">4/20/2023</div>
       <div className="show-title">{show.showname}</div>
       <div className="show-description">{show.description}</div>
+      {/* TODO: the tags should probably sit at the bottom of the container. */}
+      {/* TODO: these should only be visible for archived shows. */}
+      <div>{tags}</div>
     </div>
   )
 }
