9865928c3dff167838e6e4c9cedee7d38839445a
antons <anton@antrona.local>
2023-12-17 17:53:41 -0500

    add rel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,7 +21,9 @@ const FOOTER_LINK_DATA = [
 const Footer = () => {
   const footerLinkContent = FOOTER_LINK_DATA.map((data, index) => (
     <div className="footer-link" key={index}>
-      <a  target="_blank" href={data.link}>{data.title}</a>
+      <a rel="noreferrer" target="_blank" href={data.link}>
+        {data.title}
+      </a>
     </div>
   ))
   return (
