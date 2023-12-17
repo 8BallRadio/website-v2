19c265aa2a32cdbaa2dce6133a0500302070b3e0
M. Aden <aden781@gmail.com>
2023-12-17 17:19:28 -0500

    feat: update navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,17 @@ import React from 'react'
 
 import Player from './Player'
 import NavBar from './NavBar'
+import Logo from '../../assets/logo.png'
 
 import '../../styles.css'
+import NavLink from './NavLink'
 
 const Header = () => {
   return (
     <header>
+      <NavLink to="/">
+        <img src={Logo} style={{ width: '20%' }} />
+      </NavLink>
       <Player url={'https://eightball.out.airtime.pro/eightball_a'} />
       <NavBar />
     </header>
