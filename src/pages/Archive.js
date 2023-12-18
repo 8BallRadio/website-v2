dd46aa02979d0579c9902a8c60b0c41c4e3880b2
M. Aden <aden781@gmail.com>
2023-12-18 18:36:58 -0500

    wip: archive sets from mixcloud

--- src/pages/Archive.js
+++ src/pages/Archive.js
@@ -1,11 +1,52 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import ArchiveCard from '../components/ArchiveCard'
 import '../styles.css'
 
 const Archive = () => {
+  const [fetchError, setFetchError] = useState(null)
+  const [shows, setShows] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(0)
+
+  const MIXCLOUD_API = `https://api.mixcloud.com/8ballradio/feed/?offset=${page}`
+
+  const fetchData = async () => {
+    setLoading(true)
+    setFetchError(null)
+
+    try {
+      const res = await fetch(MIXCLOUD_API)
+      const { data } = await res.json()
+
+      setShows(data)
+      setFetchError(null)
+      console.log('data: ', data)
+    } catch (error) {
+      setFetchError(error)
+    } finally {
+      setLoading(false)
+      setPage(prevPage => prevPage + 20)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div>
-      <div className="content-header">Archive</div>
+      <div className="content-header">archive</div>
+      {fetchError && <p>{fetchError}</p>}
+      {shows && (
+        <>
+          <div className="shows-grid">
+            {shows.map((show, index) => (
+              <ArchiveCard key={index} show={show} />
+            ))}
+          </div>
+        </>
+      )}
+      {loading && <p>Loading</p>}
     </div>
   )
 }
